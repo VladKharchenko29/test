30f1646809cdbbe09434c2a6ff7bfcb925493a8f
VladKharchenko29 <vlad.mego@icloud.com>
2021-03-23 16:58:19 +0100

    test this is

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int min = Integer.MIN_VALUE;
+        int max = Integer.MAX_VALUE;
+
+        short minShort = Short.MIN_VALUE;
+        short maxShort = Short.MAX_VALUE;
+
+        System.out.println(minShort);
+    }
+}
